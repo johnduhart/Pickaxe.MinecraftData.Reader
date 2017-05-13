a8d901e429cb62d11d84c47fc3aadb0124dd7815
John Du Hart <john@johnduhart.me>
2017-05-13 15:11:49 -0400

    Forgot to commit the modifed files

--- Pickaxe.MinecraftData.Reader/DataFolderHelper.cs
+++ Pickaxe.MinecraftData.Reader/DataFolderHelper.cs
@@ -4,26 +4,6 @@ using Pickaxe.MinecraftData.Reader.Exceptions;
 
 namespace Pickaxe.MinecraftData.Reader
 {
-    internal static class PathConstants
-    {
-        public const string DataFolder = "data";
-
-        public const string DataPaths = "dataPaths.json";
-
-        public const string Biomes = "biomes.json";
-        public const string Blocks = "blocks.json";
-        public const string Effects = "effects.json";
-        public const string Enchantments = "enchantments.json";
-        public const string Entities = "entities.json";
-        public const string Instruments = "instruments.json";
-        public const string Items = "items.json";
-        public const string Materials = "materials.json";
-        public const string Protocol = "protocol.json";
-        public const string Recipes = "recipes.json";
-        public const string Version = "version.json";
-        public const string Windows = "windows.json";
-    }
-
     internal static class DataFolderHelper
     {
         // TODO: Caching
